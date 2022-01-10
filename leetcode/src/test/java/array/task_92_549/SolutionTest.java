ed25855ecdd1edc020caa2ba035003d47eba2bb7
Admin <kostindeniska2014@gmail.com>
2022-01-10 20:40:27 +0200

    Task Amicable_numbers: * added one solution for this task

---
+++ leetcode/src/test/java/array/task_92_549/SolutionTest.java
@@ -0,0 +1,37 @@
+package array.task_92_549;
+
+import array.task_92_549.solution.Solution92_549;
+import array.task_92_549.solution.impl.SolutionImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("array.task_92_549.Solution")
+public class SolutionTest {
+
+    private final Solution92_549 solution = new SolutionImpl();
+
+    /**
+     *
+     */
+    @Test
+    public void test() {
+        int[] array = {2,2,1};
+        int actualResult = solution.singleNumber(array);
+        Assertions.assertEquals(1, actualResult);
+    }
+
+    @Test
+    public void test2() {
+        int[] array = {1};
+        int actualResult = solution.singleNumber(array);
+        Assertions.assertEquals(1, actualResult);
+    }
+
+    @Test
+    public void test3() {
+        int[] array = {4,5,5,1,2,2,1};
+        int actualResult = solution.singleNumber(array);
+        Assertions.assertEquals(4, actualResult);
+    }
+}
---
+++ tasks/src/main/java/friendly_numbers/README.md
@@ -0,0 +1,6 @@
+## Friendly numbers (Amicable numbers)
+
+Two different natural numbers are called friendly if the first of them is equal to the sum of the divisors of the second number, except for the second number itself, and the second is equal to the sum of the divisors of the first number, except for the very first number. Find all pairs of friendly numbers, both of which belong to the interval from M to N.
+
+Amicable numbers are two different numbers so related that the sum of the proper divisors of each is equal to the other number.
+The first ten amicable pairs are: (220, 284), (1184, 1210), (2620, 2924), (5020, 5564), (6232, 6368), (10744, 10856), (12285, 14595), (17296, 18416), (63020, 76084), and (66928, 66992).
