97d6a2fa4630b999a920d0748b17926dd543143a
Admin <kostindeniska2014@gmail.com>
2022-01-15 19:57:48 +0200

    LeetTask-92-674

---
+++ leetcode/src/main/java/array/task_92_674/solution/impl/SolutionImpl.java
@@ -0,0 +1,37 @@
+package array.task_92_674.solution.impl;
+
+import array.task_92_674.solution.Solution92_674;
+
+import java.util.Arrays;
+
+/**
+ * Runtime: 4 ms
+ * Memory Usage: 40.3 MB
+ */
+public class SolutionImpl implements Solution92_674 {
+
+    @Override
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int intersectedNumber = 0;
+        int[] intersectedArray = new int[Math.min(nums1.length, nums2.length)];
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                intersectedArray[intersectedNumber] = nums1[i];
+                intersectedNumber++;
+                i++;
+                j++;
+
+            }
+        }
+        return Arrays.copyOfRange(intersectedArray, 0, intersectedNumber);
+    }
+}
---
+++ leetcode/src/test/java/array/task_92_674/solution/impl/SolutionTest.java
@@ -0,0 +1,57 @@
+package array.task_92_674.solution.impl;
+
+import array.task_92_674.solution.Solution92_674;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("array.task_92_674.Solution")
+class SolutionTest {
+
+    private final Solution92_674 solution = new SolutionImpl();
+
+    @Test
+    public void test1() {
+        int[] array1 = {1,2,2,1};
+        int[] array2 = {2,2};
+        int[] expectedResult = {2,2};
+        int[] actualResult = solution.intersect(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test2() {
+        int[] array1 = {4,9,5};
+        int[] array2 = {9,4,9,8,4};
+        int[] expectedResult = {4,9};
+        int[] actualResult = solution.intersect(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test3() {
+        int[] array1 = {1,2};
+        int[] array2 = {1,1};
+        int[] expectedResult = {1};
+        int[] actualResult = solution.intersect(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test4() {
+        int[] array1 = {2,6,2,9,1};
+        int[] array2 = {7,1};
+        int[] expectedResult = {1};
+        int[] actualResult = solution.intersect(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    /*
+    [2,6,2,9,1]
+    [7,1]
+     */
+}
