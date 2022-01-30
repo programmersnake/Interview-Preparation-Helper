d5cd2c7e969a1cc43c96f38c4973fa89b43acef0
Admin <kostindeniska2014@gmail.com>
2022-01-30 22:16:30 +0200

    LeetTask-92-770 rotate image - solution; - tests; - description and file with examples of shifting

--- leetcode/src/main/java/array/task_92_770/solution/impl/Solution.java
+++ leetcode/src/main/java/array/task_92_770/solution/impl/Solution.java
@@ -2,9 +2,52 @@ package array.task_92_770.solution.impl;
 
 import array.task_92_770.solution.ISolution;
 
+/**
+ * My solution`s results:
+ * - Runtime: 0 ms
+ * - Memory Usage: 42.9 MB
+ */
 public class Solution implements ISolution {
     @Override
     public void rotate(int[][] matrix) {
+        int sideLength = matrix.length;
+        int countOfMaxIteration = sideLength / 2;
+        int globalIteration = 0;
+        while (globalIteration < countOfMaxIteration) {
+            shiftRight(matrix, globalIteration, sideLength - globalIteration);
+            shiftDown(matrix, globalIteration, sideLength - globalIteration);
+            shiftLeft(matrix, globalIteration, sideLength - globalIteration);
+            globalIteration++;
+        }
+    }
 
+    private void shiftRight(int[][] matrix, int iteration, int maxIndex) {
+        for (int i = iteration; i < maxIndex - 1; i++) {
+            int tempNumberValue = matrix[iteration][i];
+            matrix[iteration][i] = matrix[i][maxIndex - 1];
+            matrix[i][maxIndex - 1] = tempNumberValue;
+        }
+    }
+
+    private void shiftDown(int[][] matrix, int iteration, int maxIndex) {
+        for (int i = 0; i < maxIndex - 1; i++) {
+            int tempNumberValue = matrix[iteration][iteration + i];
+            if (i + iteration >= maxIndex - 1) {
+                break;
+            }
+            matrix[iteration][iteration + i] = matrix[maxIndex - 1][maxIndex - 1 - i];
+            matrix[maxIndex - 1][maxIndex - 1 - i] = tempNumberValue;
+        }
+    }
+
+    private void shiftLeft(int[][] matrix, int iteration, int maxIndex) {
+        for (int i = 0; i < maxIndex - 1; i++) {
+            int tempNumberValue = matrix[iteration][iteration + i];
+            if (i + iteration >= maxIndex - 1) {
+                break;
+            }
+            matrix[iteration][iteration + i] = matrix[maxIndex - 1 - i][iteration];
+            matrix[maxIndex - 1 - i][iteration] = tempNumberValue;
+        }
     }
 }
--- leetcode/src/test/java/array/task_92_770/solution/impl/SolutionTest.java
+++ leetcode/src/test/java/array/task_92_770/solution/impl/SolutionTest.java
@@ -26,4 +26,46 @@ class SolutionTest {
         Assertions.assertArrayEquals(expectedResult, inputArray);
     }
 
+    @Test
+    public void test3() {
+        int[][] inputArray = {
+                {26, 27, 28, 11, 17},
+                {29, 30, 31, 10, 18},
+                {13, 32, 33, 34, 19},
+                {15, 14, 12, 16, 20},
+                {21, 22, 23, 24, 25}
+        };
+        int[][] expectedResult = {
+                {21, 15, 13, 29, 26},
+                {22, 14, 32, 30, 27},
+                {23, 12, 33, 31, 28},
+                {24, 16, 34, 10, 11},
+                {25, 20, 19, 18, 17}
+        };
+        solution.rotate(inputArray);
+        Assertions.assertArrayEquals(expectedResult, inputArray);
+    }
+
+    @Test
+    public void test4() {
+        int[][] inputArray = {
+                {26, 27, 28, 11, 17, 37},
+                {29, 30, 31, 10, 18, 38},
+                {13, 32, 33, 34, 19, 39},
+                {15, 14, 12, 16, 20, 29},
+                {21, 22, 23, 24, 25, 30},
+                {31, 32, 33, 34, 35, 36}
+        };
+        int[][] expectedResult = {
+                {31, 21, 15, 13, 29, 26},
+                {32, 22, 14, 32, 30, 27},
+                {33, 23, 12, 33, 31, 28},
+                {34, 24, 16, 34, 10, 11},
+                {35, 25, 20, 19, 18, 17},
+                {36, 30, 29, 39, 38, 37}
+        };
+        solution.rotate(inputArray);
+        Assertions.assertArrayEquals(expectedResult, inputArray);
+    }
+
 }
