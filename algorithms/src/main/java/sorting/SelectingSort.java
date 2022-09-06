28b39e3d47c96f89658dcf02d3b1c30a6dc27896
Admin <kostindeniska2014@gmail.com>
2022-09-06 19:28:07 +0300

    SelectingSort

---
+++ algorithms/src/main/java/sorting/SelectingSort.java
@@ -0,0 +1,20 @@
+package sorting;
+
+public class SelectingSort {
+
+    public int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) { // if < decreasing order
+                    minIndex = j;
+                }
+            }
+            int tempValue = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = tempValue;
+        }
+        return array;
+    }
+
+}
